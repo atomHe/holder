4ee39febda4db105f6c22edce187a6aabe9cc1b6
atomHe <faceplay@yandex.ru>
2016-09-06 15:58:34 +0400

    test

---
+++ public_html/index.php
@@ -0,0 +1,289 @@
+<?php
+	include $_SERVER['DOCUMENT_ROOT'] . "/" . 'manager/config.core.php';
+	include 'module/lib/less-handler.php';
+	include $_SERVER['DOCUMENT_ROOT'] . "/" . '/module/lib/Mobile_Detect.php';
+?>
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="/favicon.ico" rel="icon" sizes="16x16 32x32 48x48 64x64" type="image/vnd.microsoft.icon">
+    <title>Holdermenu</title>
+
+  	<link href="template/css/animate.css" rel="stylesheet" type="text/css">
+    <link href="template/css/magnific-popup.css" rel="stylesheet">
+    <link href="template/css/artemida.css" rel="stylesheet">
+    <link href="template/css/main.css" rel="stylesheet">
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <meta name="yandex-verification" content="" />
+    <meta name="google-site-verification" content="" />
+</head>
+
+<body>
+
+<header>
+    <div class="container">
+        <div class="row">
+        </div>
+    </div>
+</header>
+
+<main>
+
+<section id="main_screen" class="screen">
+<div class="container">
+<div class="row">
+
+	<div class="title blue">Энергия для вашего бизнеса</div>
+	<div class="title black">Настольное меню для зарядки любых мобильных устройств</div>
+	<div class="title small">Идеально подойдет для ресторанов, кафе, кинотеатров, фитнес-центров и прочего</div>
+
+	<button class="btn arrow" data-text="Узнать подробнее">
+		<div>
+			Узнать подробнее <span class="arrow"></span>
+		</div>
+	</button>
+
+	<div class="mockup"></div>
+
+</div>
+</div>
+</section>
+
+<section id="client_screen" class="screen">
+<div class="container">
+ <div class="row">
+	<div class="title">ДЛЯ КОГО ПОДХОДИТ
+		<span>НАСТОЛЬНОЕ МЕНЮ</span>
+	</div>
+
+	<div class="block col-4">
+		<div class="img-1">
+			<span>Кафе, рестораны, клубы и бары</span>
+
+			<div class="block-hover">
+				<div class="block-title">Автомастерские, автомойки и автосалоны</div>
+
+				<div class="block-text">Улучшит качество обслуживания клиентов. Дополнительные рекламные места для партнеров, опережайте своих конкурентов!</div>
+
+				<div class="block-text">Используются в залах ожидания, RECEPTION</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="block col-4">
+		<div class="img-2">
+			<span>Кафе, рестораны, клубы и бары</span>
+
+			<div class="block-hover">
+				<div class="block-title">Автомастерские, автомойки и автосалоны</div>
+
+				<div class="block-text">Улучшит качество обслуживания клиентов. Дополнительные рекламные места для партнеров, опережайте своих конкурентов!</div>
+
+				<div class="block-text">Используются в залах ожидания, RECEPTION</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="block col-4">
+		<div class="img-3">
+			<span>Кафе, рестораны, клубы и бары</span>
+
+			<div class="block-hover">
+				<div class="block-title">Автомастерские, автомойки и автосалоны</div>
+
+				<div class="block-text">Улучшит качество обслуживания клиентов. Дополнительные рекламные места для партнеров, опережайте своих конкурентов!</div>
+
+				<div class="block-text">Используются в залах ожидания, RECEPTION</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="block col-4">
+		<div class="img-4">
+			<span>Кафе, рестораны, клубы и бары</span>
+
+			<div class="block-hover">
+				<div class="block-title">Автомастерские, автомойки и автосалоны</div>
+
+				<div class="block-text">Улучшит качество обслуживания клиентов. Дополнительные рекламные места для партнеров, опережайте своих конкурентов!</div>
+
+				<div class="block-text">Используются в залах ожидания, RECEPTION</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="block col-4">
+		<div class="img-5">
+			<span>Кафе, рестораны, клубы и бары</span>
+
+			<div class="block-hover">
+				<div class="block-title">Автомастерские, автомойки и автосалоны</div>
+
+				<div class="block-text">Улучшит качество обслуживания клиентов. Дополнительные рекламные места для партнеров, опережайте своих конкурентов!</div>
+
+				<div class="block-text">Используются в залах ожидания, RECEPTION</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="block col-4">
+		<div class="img-6">
+			<span>Кафе, рестораны, клубы и бары</span>
+
+			<div class="block-hover">
+				<div class="block-title">Автомастерские, автомойки и автосалоны</div>
+
+				<div class="block-text">Улучшит качество обслуживания клиентов. Дополнительные рекламные места для партнеров, опережайте своих конкурентов!</div>
+
+				<div class="block-text">Используются в залах ожидания, RECEPTION</div>
+			</div>
+		</div>
+	</div>
+	</div>
+</div>
+</section>
+
+<section id="features_screen" class="screen">
+<div class="container">
+<div class="row">
+
+	<div class="title">
+		<span>инновационный источник энергии</span>
+		для вашего бизнеса
+		<small>Увеличивает количество лояльных и довольных клиентов. Приносит допольнительный приток клиентов и рост прибыли.</small>
+	</div>
+
+	<div class="col-3 left">
+		<div class="block">
+			<div class="block-title">Эффективно</div>
+			<div class="block-text">2 в 1. Настольное меню и зарядное устройство для ваших посетителей.</div>
+			<div class="block-text">Теперь популярность вашего заведения будет расти, а вместе с ней и ваша прибыль.</div>
+		</div>
+		<div class="block">
+			<div class="block-title">Эффективно</div>
+			<div class="block-text">2 в 1. Настольное меню и зарядное устройство для ваших посетителей.</div>
+			<div class="block-text">Теперь популярность вашего заведения будет расти, а вместе с ней и ваша прибыль.</div>
+			<div class="block-text">Теперь популярность вашего заведения будет расти, а вместе с ней и ваша прибыль.</div>
+		</div>
+	</div>
+	<div class="col-6 center"></div>
+	<div class="col-3 right">
+		<div class="block">
+			<div class="block-title">Эффективно</div>
+			<div class="block-text">2 в 1. Настольное меню и зарядное устройство для ваших посетителей.</div>
+		</div>
+		<div class="block">
+			<div class="block-title">Эффективно</div>
+			<div class="block-text">2 в 1. Настольное меню и зарядное устройство для ваших посетителей.</div>
+			<div class="block-text">2 в 1. Настольное меню и зарядное устройство для ваших посетителей.</div>
+			<div class="block-text">2 в 1. Настольное меню и зарядное устройство для ваших посетителей.</div>
+		</div>
+		<div class="block">
+			<div class="block-title">Эффективно</div>
+			<div class="block-text">2 в 1. Настольное меню и зарядное устройство для ваших посетителей.</div>
+		</div>
+	</div>
+
+</div>
+	<div class="offer">
+		<div class="offer-text">Нужен новый источник энергии для вашего бизнеса? Оформите заказ на образец, доставим в течение 7дней.</div>
+
+		<button class="btn cart" data-text="Заказать образец">
+			<div>
+				<span class="cart"></span>
+				<span class="text">Заказать образец</span>
+			</div>
+		</button>
+	</div>
+
+</div>
+</section>
+
+<section id="advantages_screen" class="screen">
+	<div class="container">
+		<div class="title">
+			ПРЕИМУЩЕСТВА
+			<span>Холдер меню станет не заменимым помощником в вашем заведении.</span>
+		</div>
+		<div class="row">
+
+			<div class="block col-4">
+				<div class="ico_wrapper">
+					<div class="ico-1"></div>
+				</div>
+				<div class="block-title">Новые клиенты</div>
+				<div class="block-text">С бесплатной подзарядкой увелится поток ваших клиентов</div>
+			</div>
+			<div class="block col-4">
+				<div class="ico">
+					<div class="ico-2"></div>
+				</div>
+				<div class="block-title">Гарантия качества</div>
+				<div class="block-text">Фирменные батареи SAMSUNG гарантируют длительный и бесперебойный срок службы</div>
+			</div>
+			<div class="block col-4">
+				<div class="ico">
+					<div class="ico-3"></div>
+				</div>
+				<div class="block-title">Безопасность</div>
+				<div class="block-text">С бесплатной подзарядкой увелится поток ваших клиентов</div>
+			</div>
+
+			<div class="block col-4">
+				<div class="ico">
+					<div class="ico-4"></div>
+				</div>
+				<div class="block-title">Совместимость</div>
+				<div class="block-text">Кабель с разъемами - для Iphone (lighning), Android устройств (micro-usb) и 30-Pin</div>
+			</div>
+			<div class="block col-4">
+				<div class="ico">
+					<div class="ico-5"></div>
+				</div>
+				<div class="block-title">Удобная оплата</div>
+				<div class="block-text">Оплату можно произвести как наличными, так и безналичным расчетом</div>
+			</div>
+			<div class="block col-4">
+				<div class="ico">
+					<div class="ico-6"></div>
+				</div>
+				<div class="block-title">Быстрая доставка</div>
+				<div class="block-text">Оперативная доставка в любую точку России</div>
+			</div>
+
+
+		</div>
+	</div>
+	</div>
+	</div>
+</section>
+</main>
+<footer>
+	<div class="container">
+		<div class="row">
+		</div>
+	</div>
+</footer>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="template/js/jquery.bxslider.js"></script>
+<script src="template/js/simplyscroll.js"></script>
+<script src="template/js/jquery.magnific-popup.min.js"></script>
+<script src="template/js/jquery.maskedinput.js"></script>
+<script src="template/js/anchor.js"></script>
+<script src="template/js/viewportchecker.js"></script>
+<script src="template/js/main.js"></script>
+
+</body>
+</html>
